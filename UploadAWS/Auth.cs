b0c903955dbdbc2a8274f3fd05e2d9d07889e772
Thales Lima <thales@inovaebiz.com.br>
2023-09-06 14:22:35 -0300

    Subindo o Auth.cs sem as credenciais

---
+++ UploadAWS/Auth.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UploadAWS
+{
+    public class Auth
+    {
+        public static string awsKey = "";
+        public static string awsSecret = "";
+        public static string awsBucket = "";
+        public static string awsFullBucketUrl = "";
+    }
+}
